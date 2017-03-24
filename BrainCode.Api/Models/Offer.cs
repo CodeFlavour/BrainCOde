d710d6d7606c1b8683c1002abcd0d62d3a5db080
bajurar <bajurar@ee.pw.edu.pl>
2017-03-24 22:04:24 +0100

    Modele oferty i parametru filtra

---
+++ BrainCode.Api/Models/Offer.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BrainCode.Api.Models
+{
+    public class Offer
+    {
+        public long ID { get; set; }
+
+        public string Name { get; set; }
+
+        public decimal? BuyNowPrice { get; set; }
+
+        public decimal BiddingPrice { get; set; }
+
+        public DateTime OfferEndDate { get; set; }
+
+        //public byte[] Photo { get; set; }
+    }
+}
---
+++ BrainCode.Api/Models/Parameter.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BrainCode.Api.ViewModels
+{
+    public class Parameter
+    {
+        public string ParameterName { get; set; }
+        
+        public string ParameterValue { get; set; }
+    }
+}
