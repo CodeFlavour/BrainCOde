46dd19dde671271bb1ae4fd4efec0c03d84ab2c4
bajurar <bajurar@ee.pw.edu.pl>
2017-03-25 01:14:48 +0100

    Wyszukiwanie działające częściowo

--- BrainCode.Api/Models/Offer.cs
+++ BrainCode.Api/Models/Offer.cs
@@ -2,21 +2,37 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Newtonsoft.Json.Linq;
+using System.Globalization;
 
 namespace BrainCode.Api.Models
 {
     public class Offer
     {
-        public long ID { get; set; }
+        public Offer(JToken item)
+        {
+            this.ID = (string)item["id"];
+            this.Name = (string)item["name"];
+            this.BuyNowPrice = (bool)item["buyNow"] == true ? item["prices"]["buyNow"]["amount"].ToObject<decimal?>() : null;
+            //if ((bool)item["auction"])
+            //{
+            //    decimal.TryParse((string)item["prices"]["current"])
+            //}
+            this.BiddingPrice = (bool)item["auction"] == true ? item["prices"]["current"]["amount"].ToObject<decimal?>() : null;
+            this.OfferEndDate = (DateTime?)item["endingAt"];
+            this.PhotoUrl = (string)(item["images"][0]["url"]);
+        }
+
+        public string ID { get; set; }
 
         public string Name { get; set; }
 
         public decimal? BuyNowPrice { get; set; }
 
-        public decimal BiddingPrice { get; set; }
+        public decimal? BiddingPrice { get; set; }
 
-        public DateTime OfferEndDate { get; set; }
+        public DateTime? OfferEndDate { get; set; }
 
-        //public byte[] Photo { get; set; }
+        public string PhotoUrl { get; set; }
     }
 }
---
+++ BrainCode.Api/Services/SearchService.cs
@@ -0,0 +1,79 @@
+﻿using BrainCode.Api.Helpers;
+using BrainCode.Api.Models;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace BrainCode.Api.Services
+{
+    public class SearchService
+    {
+        private string _url = "https://allegroapi.io/offers";
+
+        //private Dictionary<string, string> _headers = new Dictionary<string, string>();
+
+        public async Task<List<Offer>> SearchOffers(string title, string searchPhrase, List<Parameter> parameters)
+        {
+            var _headers = new List<Header>();
+            var requestURL = _url;
+            List<string> urlParameters = new List<string>();
+            urlParameters.Add("country.code=PL");
+            if (!string.IsNullOrEmpty(searchPhrase))
+            {
+                urlParameters.Add("phrase=" + searchPhrase);
+            }
+            if (parameters != null)
+            {
+                parameters.ForEach(x => urlParameters.Add(x.ParameterName + "=" + x.ParameterValue));
+            }
+
+            HttpClient client = new HttpClient();
+            _headers.Add(new Header("User-Agent", "hackaton2017 (Client-Id 656cbe47-b17d-46c2-bae1-3222c8777d5b) Platform"));
+            _headers.Add(new Header("Api-Key", "eyJjbGllbnRJZCI6ImE0MWY1YjJhLThlODctNGI4Yi1iNmZlLTc0Y2M3NjM3MjBkNyJ9.ogVV_a9RUOMa1OWFZOTmgTkdk-U37vTliDCBUQ1YySU="));
+            _headers.Add(new Header("Accept", "application / vnd.allegro.public.v1+json"));
+            Header bearerHeader = await BearerHelper.GetBearerHeader();
+            _headers.Add(bearerHeader);
+
+
+            for (int i = 0; i< urlParameters.Count; i++)
+            {
+                requestURL += (i == 0 ? "?" : "&") + urlParameters[i];
+            }
+            var request = new HttpRequestMessage()
+            {
+                RequestUri = new Uri(requestURL),
+                Method = HttpMethod.Get,
+            };
+            
+            foreach (var header in _headers)
+            {
+                request.Headers.Add(header.Name, header.Value);
+            }
+
+            var response = await client.SendAsync(request);
+            string responseObject = await response.Content.ReadAsStringAsync();
+            if (response.StatusCode != System.Net.HttpStatusCode.OK)
+            {
+                throw new Exception(response.ReasonPhrase);
+            }
+            var responseJsonObject = (JObject)JsonConvert.DeserializeObject(responseObject);
+            List<Offer> offers = Map(responseJsonObject["offers"]);
+            //var rVal = new PagedResponse<Offer>(responseJsonObject[""], responseJsonObject[""], offers);
+            return offers;
+        }
+
+        private List<Offer> Map(JToken jToken)
+        {
+            List<Offer> rVal = new List<Offer>();
+            foreach (var item in jToken)
+            {
+                rVal.Add(new Offer(item));
+            }
+            return rVal;
+        }
+    }
+}
